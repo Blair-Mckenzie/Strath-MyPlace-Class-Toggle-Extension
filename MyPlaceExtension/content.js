ef946d23959b5806e389b5bc383fca32abb431a6
Blair-Mckenzie <wdb17134@uni.strath.ac.uk>
2022-03-02 00:03:16 +0000

    More reliable with the addition of timeouts

--- MyPlaceExtension/content.js
+++ MyPlaceExtension/content.js
@@ -3,15 +3,16 @@ const classNames = [" 19520 / EM501: Group Project", " 19520: Group Project", " 
 
 //Making sure the page has completed loading to access the LI containing
 // all the items the student has selected. Each item in the list comapared
-// against student selected classes only   
-document.addEventListener("readystatechange", (e) => {
-    if (e.target.readyState === 'complete') {
-        let ul = document.querySelector("ul.list-group.classlist");
-        ul.childNodes.forEach(childNode => {
-            if (!classNames.includes(childNode.innerText)) {
-                childNode.textContent = '';
-            }})
-    }
-});
+// against student selected classes only
+window.onload = (e) => {
+    let timeout = setTimeout(() => {
+            let ul = document.querySelector("ul.list-group.classlist");
+            ul.childNodes.forEach(childNode => {
+                if (!classNames.includes(childNode.innerText)) {
+                    childNode.textContent = '';
+                }})
+        // }
+    },1000);
+};
 
 
